e377d270e9bcf5ba7958d2a9c0cc3bbbce2e16d9
imaftei <imaftei@amazon.com>
2020-01-07 11:58:24 +0200

    stuff

--- instant.py
+++ instant.py
@@ -1,45 +1,47 @@
 import gym, keras, numpy as np
+import matplotlib.pyplot as plt
+
+from gym_wrapper import *
+
+frames_used = 4
+max_replay_size = 500
 
 def model_init():
+    std_dev = 0.001
     model = keras.models.Sequential()
-    model.add(keras.layers.Conv2D(4, (3,3), strides=(1, 1), padding = "same", activation='elu', input_shape=(98, 72, 2)))
-    model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
-    model.add(keras.layers.Conv2D(4, (3,3), strides=(1, 1), padding = "same", activation='elu'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(4, 4)))
-    model.add(keras.layers.Conv2D(8, (3,3), strides=(1, 1), padding = "same", activation='elu'))
-    model.add(keras.layers.MaxPooling2D(pool_size=(4, 4)))
-    model.add(keras.layers.Conv2D(16, (3,1), strides=(1, 1), activation='elu'))
+    model.add(keras.layers.Conv2D(32, (8,8), strides=(4, 4), padding = "same", activation='elu', input_shape=(84, 84, frames_used), 
+                                  kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=std_dev, seed=None), bias_initializer='zeros'))
+    model.add(keras.layers.Conv2D(32, (3,3), strides=(3, 3), padding = "same", activation='elu', 
+                                  kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=std_dev, seed=None), bias_initializer='zeros'))
+    model.add(keras.layers.Conv2D(64, (3,3), strides=(1, 1), padding = "same", activation='elu', 
+                                  kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=std_dev, seed=None), bias_initializer='zeros'))
     model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(units=16, activation='elu'))
-    model.add(keras.layers.Dense(units=3, activation='elu'))
-    
+    model.add(keras.layers.Dense(units=256, activation='elu', 
+                                  kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=std_dev, seed=None), bias_initializer='zeros'))
+    model.add(keras.layers.Dense(units=3, activation='elu', 
+                                  kernel_initializer=keras.initializers.RandomNormal(mean=0.0, stddev=std_dev, seed=None), bias_initializer='zeros'))
     print(model.summary())
     return model
 
-decay = 0.95
-
-def image_prep(obs):
-    obs = obs[0:-14,8:-8]
-    obs = obs[0::2,0::2]
-    obs = np.expand_dims(obs, axis=0)
-    obs = np.mean(obs, axis=3, keepdims = True)
-    obs[obs > 0] = 1
-    return obs
-
-env = gym.make('Breakout-v0')
+env = MainGymWrapper.wrap(gym.make('BreakoutDeterministic-v4'))
 env.reset()
-def train(model, use_random_actions, repetitions=100, learning_rate=0.01):
-    model.compile(loss='mse',
-              optimizer=keras.optimizers.SGD(lr=learning_rate))
+
+replays = []
+def train(model, random_action_chance, repetitions=100, learning_rate=0.01, number_of_repetitions_after_death = 50, reward_decay = 0.95):
+    print('Training with repetetitions = ' + str(repetitions) + ', learning_rate = ' + str(learning_rate) + ', random_action_chance = ' + str(random_action_chance))
+    model.compile(loss='mse', optimizer=keras.optimizers.SGD(lr=learning_rate))
+    sum_scores = 0
     for i in range(repetitions):
         done = False
-        observation = image_prep(env.reset())
-        last_observation = observation
-        prediction = model.predict(np.concatenate((observation,last_observation),axis=3))
+        observation = env.reset()
+        observation = np.moveaxis(observation, 0, 2)
+        observation = np.reshape(observation, (1,84,84,4))
+        replay = [observation]
+        prediction = model.predict(replay[0])
         a = np.argmax(prediction)+1
-        print(prediction)
-        sum = 0
+        score = 0
         j = 0
+        predictions = []
         while not done:
             j += 1
             env.render()
@@ -48,32 +50,50 @@ def train(model, use_random_actions, repetitions=100, learning_rate=0.01):
                 print("Forced Start")
             next_observation, reward, done, info = env.step(a)
             if reward > 0 or a == 1:
-                sum += reward
+                score += reward
                 j = 0
-            next_observation = image_prep(next_observation)
-            prediction = model.predict(np.concatenate((next_observation,observation),axis=3))
-            print(prediction)
+            next_observation = np.moveaxis(next_observation, 0, 2)
+            next_observation = np.reshape(next_observation, (1,84,84,4))
+            replay.append(next_observation)
+            old_prediction = prediction
+            prediction = model.predict(replay[-1])
+            print('\r', prediction, sep='', end='', flush=True)
             old_a = a
-            if use_random_actions:
+            if np.random.rand() < random_action_chance:
                 a = np.random.randint(1, 4)
             else:
                 a = np.argmax(prediction)+1
             if done:
-                prediction = np.zeros([1,3])
-                epochs = 50
+                old_prediction = np.zeros([1,3])
+                epochs = number_of_repetitions_after_death
+                model.save('breakout.h5')
             else:
-                prediction[0][old_a-1] = reward + decay * np.max(prediction)
+                old_prediction[0][old_a-1] = reward + reward_decay * np.max(prediction)
                 epochs = 1
-            model.fit(np.concatenate((observation,last_observation),axis=3), prediction, epochs = epochs, verbose = False)
-            last_observation = observation
-            observation = next_observation
-        print(i, sum)
-        if (i+1) % 100 == 0:
-            model.save('breakout.h5')
+            predictions.append(old_prediction)
+            model.fit(replay[-2], old_prediction, epochs = epochs, verbose = False)
+        remember(predictions, replay)
+        print()
+        sum_scores += score
+        print(i, score, sum_scores/(i+1))
+
+def remember(prediction, replay):
+    if len(replays) < max_replay_size:
+        replays.append((prediction, replay))
+    else:
+        replays.pops(0)
+        replays.append((prediction, replay))
 
 model = model_init()
-train(model, True, repetitions=10, learning_rate=0.01)
-train(model, False, repetitions = 1000, learning_rate=0.0001)
+train(model, 1, repetitions=20, learning_rate=0.1, number_of_repetitions_after_death = 0)
+train(model, 1, repetitions=20, learning_rate=0.01, number_of_repetitions_after_death = 0)
+train(model, 1, repetitions=20, learning_rate=0.001, number_of_repetitions_after_death = 0)
+train(model, 0.9, repetitions = 100, learning_rate=0.001, number_of_repetitions_after_death = 0)
+train(model, 0.9, repetitions = 100, learning_rate=0.0005, number_of_repetitions_after_death = 0)
+train(model, 0.8, repetitions = 100, learning_rate=0.00025, number_of_repetitions_after_death = 0)
+train(model, 0.7, repetitions = 100, learning_rate=0.0001, number_of_repetitions_after_death = 0)
+train(model, 0.6, repetitions = 100, learning_rate=0.0001, number_of_repetitions_after_death = 0)
+train(model, 0.5, repetitions = 100, learning_rate=0.0001, number_of_repetitions_after_death = 0)
 
 env.close()
 
